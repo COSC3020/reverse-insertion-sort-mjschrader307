79fe5710b8de8ef6888769ff87048124cb781176
mjschrader307 <mjschrader307@gmail.com>
2025-02-18 13:09:31 -0700

    Stability issue?

--- code.js
+++ code.js
@@ -1,8 +1,6 @@
 function insertionSortReverse(array) {
   const n = array.length;
 
-  if (n <= 1) return array;
-
   // For every element in the array (back to front)
   for (let i = n - 2; i >= 0; i--) {
     // arr[i:n-1] is sorted
@@ -10,7 +8,7 @@ function insertionSortReverse(array) {
     let val = array[i];
     let j;
 
-    for (j = i; j < n - 1 && array[j + 1] < val; j++) {
+    for (j = i; j < n - 1 && array[j + 1] <= val; j++) {
       array[j] = array[j + 1];
     }
 
