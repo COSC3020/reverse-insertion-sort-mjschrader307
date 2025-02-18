03ac0cc5011524e83863c6d4004840113c690b6e
mjschrader307 <mjschrader307@gmail.com>
2025-02-18 13:00:54 -0700

    Done; ready for review

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
 function insertionSortReverse(array) {
-    return array;
+  const n = array.length;
+
+  // For every element in the array (back to front)
+  for (let i = n - 2; i >= 0; i--) {
+    // arr[i:n-1] is sorted
+
+    let val = array[i];
+    let j;
+
+    for (j = i; j < n - 1 && array[j + 1] < val; j++) {
+      array[j] = array[j + 1];
+    }
+
+    array[j] = val;
+  }
 }
